9e32eedd2116a8e8386f7ec5b0a638ce14073826
Alexander Laut <14655890+alaut@users.noreply.github.com>
2021-11-08 11:00:22 +0100

    incorporate Timer() functionality

--- sylt/tools.py
+++ sylt/tools.py
@@ -1,6 +1,9 @@
 import numpy as np
 from scipy.stats import norm
 
+import time
+from dataclasses import dataclass
+
 def limits(x, n=2):
     """distribute points between limits of array x"""
     return np.linspace(np.nanmin(x), np.nanmax(x), n)
@@ -13,3 +16,13 @@ def statlim(x, n=2, N=3):
     """distribute points between statistical limits of array x"""
     mu, sig = norm.fit(x)
     return np.linspace(mu-N*sig, mu+N*sig, n)
+
+@dataclass
+class Timer():
+    description: str = ''
+
+    def __enter__(self):
+        self.start = time()
+
+    def __exit__(self, type, value, traceback):
+        print(f"{self.description}> {time()-self.start:0.2f} seconds")
