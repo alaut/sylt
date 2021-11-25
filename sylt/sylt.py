3c8101b1d92228e295cb6d3deae1ff78f9c7c720
Alexander Laut <14655890+alaut@users.noreply.github.com>
2021-11-25 15:34:08 +0100

    improve data management

--- sylt/sylt.py
+++ sylt/sylt.py
@@ -7,38 +7,43 @@ from sylt.tracking import Ring, Bunch, Tracker
 from sylt.tools import centers
 
 
-def benchmark_bunch_profiles(tau, t, lam, show=True, simulate=False, sig_eps=3e-6):
+def benchmark_bunch_profiles(tau, t, lam, sig_eps, show=True, simulate=False):
     """broad analysis of BLO with options to benchmark with simulation"""
 
-    fit_exp = analyze_bunch_profiles(tau, t, lam)
+    out = {}
+
+    out['exp'] = {}
+    out['exp']['fit'] = analyze_bunch_profiles(tau, t, lam)
 
     if show:
-        figs_exp = plot_bunch_profiles(tau, t, lam, fit_exp)
+        out['exp']['figs'] = plot_bunch_profiles(tau, t, lam, out['exp']['fit'])
 
     if simulate:
+        out['sim'] = {}
 
-        N = np.max(fit_exp['gaussian']['amp'] *
-                   np.sqrt(2*np.pi*fit_exp['gaussian']['var']))
-        print(f"N:{N*1e-10:0.2g} x 1e+10")
+        N = np.max(out['exp']['fit']['binomial']['amp'])
+        print(f"N:{N:0.3e}")
 
-        mu_sig_tau = fit_exp['oscillator']['mu']
+        mu_sig_tau = out['exp']['fit']['oscillator']['mu']
 
         bunch = Bunch(
             E=2.938272e9,
             sig_w=4e6,
             sig_tau=mu_sig_tau,
             n=40_000,     # 0.5% error
-            N=N, sig_eps=sig_eps,
-            eps=None, LONG_SHAPE='binomial',
+            N=N,
+            sig_eps=sig_eps,
+            LONG_SHAPE='binomial',
+            eps=None,
         )
 
         tracker = Tracker(bunch, Ring(), FIXED_MU=True)
 
-        tracker.estimate_voltage(Omega=fit_exp['oscillator']['omega']/2)
+        tracker.estimate_voltage(Omega=out['exp']['fit']['oscillator']['omega']/2)
 
         tracker.match(
             sig_tau=mu_sig_tau,
-            k=fit_exp['oscillator']['A']/mu_sig_tau,
+            k=out['exp']['fit']['oscillator']['A']/mu_sig_tau,
         )
 
         turns = np.arange(12_000)
@@ -47,25 +52,21 @@ def benchmark_bunch_profiles(tau, t, lam, show=True, simulate=False, sig_eps=3e-
         dt = np.mean(np.diff(tau))
 
         LAM = []
-        tracker.show('start')
+        print('tracking ...')
         for turn in turns:
             tracker.track()
 
             if turn % 1_000 == 0:
-                lost = tracker.clean()
-                print(f"lost {lost.sum()/lost.size*100:0.2g} % of particles")
-
-            # if turn in range(0, NUM_TURNS, 2500):
-            #     tracker.show(f"{turn}")
+                tracker.clean()
 
             lam, _ = np.histogram(tracker.bunch.tau, tau)
             LAM.append(lam/dt/tracker.bunch.n*N)
-        tracker.show('stop')
+
         LAM = np.array(LAM)
 
-        fit_sim = analyze_bunch_profiles(centers(tau), t, LAM)
+        out['sim']['fit'] = analyze_bunch_profiles(centers(tau), t, LAM)
 
         if show:
-            figs_sim = plot_bunch_profiles(centers(tau), t, LAM, fit_sim)
+            out['sim']['figs'] = plot_bunch_profiles(centers(tau), t, LAM, out['sim']['fit'])
 
-    return {'exp': {'fit': fit_exp, 'figs': figs_exp}, 'sim': {'fit': fit_sim, 'figs': figs_sim}}
+    return out
